a6860fc4a1651caa890a62f09710b8af35f22f99
Miguel Reyes <miguel.reyes@millicom.com>
2015-11-06 17:44:20 -0500

    3.3 Implement apply and unapply methods  Cookbook has a companion object. Recipe implements apply/unapply methods.
    Use unapply method:
    scala> Cookbook.pbj match {
         |   case Recipe(ingredients, directions) =>
         |     println(ingredients)
         |     println(directions)
         | }
    
    Pattern matching:
    scala> def isSimple(recipe: Recipe): Boolean = recipe match {
         |   case Recipe(_, List(_)) => true
         |   case _ => false
         | }

--- Hello/src/main/scala/safarionline/cookbook/Cookbook.scala
+++ Hello/src/main/scala/safarionline/cookbook/Cookbook.scala
@@ -1,6 +1,8 @@
 package safarionline.cookbook
 
 /**
+  * Uses a companion "object" for Recipe.
+  *
   * @author Miguel Reyes
   *         Date: 11/6/15
   *         Time: 11:20 AM
@@ -12,8 +14,34 @@ class Cookbook(val recipes: Map[String, Recipe]) {
   def this() = this(Map.empty)
 }
 
-class Recipe(ingredients: List[String] = List.empty,
-                  directions: List[String] = List.empty) {
-  println(ingredients)
-  println(directions)
+
+/**
+  * Use "private" to force the use of the Factory method "make".
+  * @param ingredients Ingredients list
+  * @param directions Directions list
+  */
+class Recipe(val ingredients: List[String],
+              val directions: List[String]) {
+}
+
+
+/**
+  * Create a singleton with a constructor method
+  */
+object Recipe {
+  def apply(ingredients: List[String] = List.empty,
+            directions: List[String] = List.empty): Recipe = {
+    new Recipe(ingredients, directions)
+  }
+
+  def unapply(recipe: Recipe): Option[(List[String], List[String])] = {
+    Some((recipe.ingredients, recipe.directions))
+  }
+
+}
+
+object Cookbook {
+  val pbj = Recipe(List("Peanut butter sandwich"), List("Add peanut butter to bread"))
+  val pancakes = Recipe(List("Pancakes"), List("Mix ingredients and bake"))
+
 }
