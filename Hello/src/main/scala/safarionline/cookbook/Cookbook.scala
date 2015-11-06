0def912c10d68f6cdf10c743a9d45457f621f587
Miguel Reyes <miguel.reyes@millicom.com>
2015-11-06 17:55:07 -0500

    3.4 Eliminate boilerplate with case classes
    scala> val r1 = Recipe(List.empty, List.empty)
    r1: safarionline.cookbook.Recipe = Recipe(List(),List())
    
    scala> val r2 = Recipe(List.empty, List.empty)
    r2: safarionline.cookbook.Recipe = Recipe(List(),List())
    
    scala> r1 == r2
    res0: Boolean = true
    
    scala> Cookbook.pbj.copy(ingredients = List())
    res1: safarionline.cookbook.Recipe = Recipe(List(),List(Add peanut butter to bread))

--- Hello/src/main/scala/safarionline/cookbook/Cookbook.scala
+++ Hello/src/main/scala/safarionline/cookbook/Cookbook.scala
@@ -1,47 +1,20 @@
 package safarionline.cookbook
 
 /**
-  * Uses a companion "object" for Recipe.
-  *
-  * @author Miguel Reyes
-  *         Date: 11/6/15
-  *         Time: 11:20 AM
-  */
-class Cookbook(val recipes: Map[String, Recipe]) {
-  println(recipes)
-
-  //Create additional constructor. It needs to call the class constructor
-  def this() = this(Map.empty)
-}
-
-
-/**
   * Use "private" to force the use of the Factory method "make".
   * @param ingredients Ingredients list
   * @param directions Directions list
   */
-class Recipe(val ingredients: List[String],
-              val directions: List[String]) {
-}
-
-
-/**
-  * Create a singleton with a constructor method
-  */
-object Recipe {
-  def apply(ingredients: List[String] = List.empty,
-            directions: List[String] = List.empty): Recipe = {
-    new Recipe(ingredients, directions)
-  }
-
-  def unapply(recipe: Recipe): Option[(List[String], List[String])] = {
-    Some((recipe.ingredients, recipe.directions))
-  }
-
+case class Recipe(ingredients: List[String],
+                  directions: List[String]) {
 }
 
 object Cookbook {
-  val pbj = Recipe(List("Peanut butter sandwich"), List("Add peanut butter to bread"))
-  val pancakes = Recipe(List("Pancakes"), List("Mix ingredients and bake"))
+  val pbj = Recipe(
+    List("Peanut butter", "Bread", "Jelly"),
+    List("Add peanut butter to bread"))
+  val bananaPancakes = Recipe(
+    List("Flour mix", "Banana"),
+    List("Mix ingredients and bake"))
 
 }
