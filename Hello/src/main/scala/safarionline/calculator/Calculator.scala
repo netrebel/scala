ff6010e420d4381935b1eb1c58d982941a70ad29
Miguel Reyes <miguel.reyes@millicom.com>
2015-11-09 16:18:28 -0500

    3.7 Expand the calculator with object-oriented style. (Part 1)  - Refactored to use objects.

--- Hello/src/main/scala/safarionline/calculator/Calculator.scala
+++ Hello/src/main/scala/safarionline/calculator/Calculator.scala
@@ -3,51 +3,74 @@ package safarionline.calculator
 import scala.collection.mutable
 
 /**
- * @author Miguel Reyes
- *         Date: 11/5/15
- *         Time: 3:32 PM
- */
+  * @author Miguel Reyes
+  *         Date: 11/5/15
+  *         Time: 3:32 PM
+  */
 object Calculator {
 
-  def handleOperator(token: String, stack: mutable.Stack[Int]): Boolean = token match {
-    case "+" =>
-      val rhs = stack.pop()
-      val lhs = stack.pop()
-      stack.push(lhs + rhs)
-      true
-    case "-" =>
-      val rhs = stack.pop()
-      val lhs = stack.pop()
-      stack.push(lhs - rhs)
-      true
-    case "*" =>
-      val rhs = stack.pop()
-      val lhs = stack.pop()
-      stack.push(lhs * rhs)
-      true
-    case "/" =>
-      val rhs = stack.pop()
-      val lhs = stack.pop()
-      stack.push(lhs / rhs)
-      true
-    case _ => false
+  trait Operator {
+    def operate(lhs: Int, rhs: Int): Int
   }
 
-  def handleNumber(token: String, stack: mutable.Stack[Int]): Boolean = try {
-    stack.push(token.toInt)
-    true
-  } catch {
-    case _: NumberFormatException => false
+  object Operator {
+
+    val operators: Map[String, Operator] =
+      Map("+" -> Add, "-" -> Subtract, "*" -> Multiply, "/" -> Divide)
+
+    def unapply(token: String): Option[Operator] = {
+      operators.get(token)
+
+    }
+  }
+
+  case object Add extends Operator {
+    override def operate(lhs: Int, rhs: Int): Int = lhs + rhs
+
+    override val toString = "+"
+  }
+
+  case object Subtract extends Operator {
+    override def operate(lhs: Int, rhs: Int): Int = lhs - rhs
+
+    override val toString = "-"
+  }
+
+  case object Multiply extends Operator {
+    override def operate(lhs: Int, rhs: Int): Int = lhs * rhs
+
+    override val toString = "*"
+  }
+
+  case object Divide extends Operator {
+    override def operate(lhs: Int, rhs: Int): Int = lhs / rhs
+
+    override val toString = "/"
+  }
+
+  object Number {
+    def unapply(token: String): Option[Int] = try {
+      Some(token.toInt)
+    } catch {
+      case _: NumberFormatException => None
+    }
   }
 
   def calculate(expression: String): Int = {
     val stack = new mutable.Stack[Int]
 
-    for (token <- expression.split(" "))
-      if (!handleOperator(token, stack) && !handleNumber(token, stack))
-        throw new IllegalArgumentException("Invalid argument: " + token)
+    for (token <- expression.split(" ")) token match {
+      case Number(num) => stack.push(num)
+      case Operator(op) =>
+        val rhs: Int = stack.pop()
+        val lhs: Int = stack.pop()
+        stack.push(op.operate(lhs, rhs))
+
+      case _ => throw new IllegalArgumentException("Illegal character" + token)
+    }
 
     stack.pop()
+
   }
 
   def main(args: Array[String]) {
