7daacd663306b55c81f08bf63c621cd78aab43dc
Miguel Reyes <miguel.reyes@millicom.com>
2015-11-09 13:21:54 -0500

    3.5 Apply polymorphism and inheritance
     scala> Cookbook.pbj.shoppingList(Map("peanut butter"-> Kilograms(1)))
     res0: List[String] = List(bread, jelly)

--- Hello/src/main/scala/safarionline/cookbook/Cookbook.scala
+++ Hello/src/main/scala/safarionline/cookbook/Cookbook.scala
@@ -5,16 +5,59 @@ package safarionline.cookbook
   * @param ingredients Ingredients list
   * @param directions Directions list
   */
-case class Recipe(ingredients: List[String],
+case class Recipe(ingredients: Map[String, Mass],
                   directions: List[String]) {
+
+  def shoppingList(kitchen: Map[String, Mass]): List[String] =
+    for {
+      (name, need) <- ingredients.toList
+      have = kitchen.getOrElse(name, Grams(0))
+      //Guard: Is the amount that we have less that what we need
+      if have.compareTo(need) < 0
+    } yield name
 }
+
+/**
+  * Use "sealed" says that no other classes outside the file are allowed to extend Mass
+  * Also known as Algebraic data type.
+  * By using abstract "defs" we can overrider them with whatever we want to in the implementations.
+  */
+sealed abstract class Mass() extends Comparable[Mass] {
+  def amount: Double
+
+  def toGrams: Grams
+
+  def compareTo(that: Mass): Int = (this.toGrams.amount - that.toGrams.amount).toInt
+}
+
+case class Grams(amount: Double) extends Mass {
+  override def toGrams: Grams = this
+
+  override def toString: String = amount + "g"
+}
+
+case class Milligrams(amount: Double) extends Mass {
+  override def toGrams: Grams = Grams(this.amount / 1000)
+
+  override def toString: String = amount + "mg"
+}
+
+case class Kilograms(amount: Double) extends Mass {
+  override def toGrams: Grams = Grams(this.amount * 1000)
+
+  override def toString: String = amount + "kg"
+}
+
 
 object Cookbook {
   val pbj = Recipe(
-    List("Peanut butter", "Bread", "Jelly"),
+    Map("peanut butter" -> new Grams(50),
+      "bread" -> new Grams(250),
+      "jelly" -> new Kilograms(0.5)),
     List("Add peanut butter to bread"))
   val bananaPancakes = Recipe(
-    List("Flour mix", "Banana"),
+    Map("flour mix" -> new Kilograms(0.5),
+      "banana" -> new Grams(500)),
     List("Mix ingredients and bake"))
 
 }
