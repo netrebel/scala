2059881177fa64e3e012021d73273ad973baabfc
Miguel Reyes <miguel.reyes@millicom.com>
2015-11-12 13:54:45 -0500

    5.4 Manipulate functions with currying

--- Hello/src/main/scala/safarionline/functions.scala
+++ Hello/src/main/scala/safarionline/functions.scala
@@ -6,7 +6,7 @@
 package object functions {
 
   /**
-    * Example: map(List(1,2,3), { a: Int => a * 2})
+    * Example: map(List(1,2,3))({ _ * 2})
     *
     * @param list Initial List
     * @param fn Function that takes parameter A and turns it into B
@@ -14,23 +14,23 @@ package object functions {
     * @tparam B Parameter
     * @return List of new elements of type B after applying the function.
     */
-  def map[A, B](list: List[A], fn: A => B): List[B] = list match {
-    case head :: tail => fn(head) :: map(tail, fn)
+  def map[A, B](list: List[A])(fn: A => B): List[B] = list match {
+    case head :: tail => fn(head) :: map(tail)(fn)
     case _ => Nil
   }
 
   /**
     *
-    * Example: filter(List(1,2,3,4,5), {a: Int => a % 2 == 0})
+    * Example: filter(List(1,2,3,4,5))({_ % 2 == 0})
     *
     * @param list Initial list
     * @param fn Function that takes parameter A and returns a Boolean
     * @tparam A Parameter to be evaluated
     * @return List of elements that match the function.
     */
-  def filter[A](list: List[A], fn: A => Boolean): List[A] = list match {
+  def filter[A](list: List[A])(fn: A => Boolean): List[A] = list match {
     case head :: tail =>
-      val rest = filter(tail, fn)
+      val rest = filter(tail)(fn)
       if (fn(head))
         head :: rest
       else
@@ -39,7 +39,7 @@ package object functions {
   }
 
   /**
-    * Example: foldLeft(List(1,2,3), 0, { (acc: Int, e: Int) => acc + e }
+    * Example: foldLeft(List(1,2,3), 0)({ (acc, e) => acc + e })
     *
     * @param list Initial list
     * @param acc Accumulator of type B
@@ -48,9 +48,9 @@ package object functions {
     * @tparam A Element of the list
     * @return Value accumulated
     */
-  def foldLeft[A, B](list: List[A], acc: B, fn: (B, A) => B): B = list match {
+  def foldLeft[A, B](list: List[A], acc: B)(fn: (B, A) => B): B = list match {
     case head :: tail =>
-      foldLeft(tail, fn(acc, head), fn)
+      foldLeft(tail, fn(acc, head))(fn)
 
     case _ => acc
 
